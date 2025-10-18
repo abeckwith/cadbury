46385837fa52dc096265ccd7cf82bb7db81c1b9c
abeckwith <anthonyrbeckwith@gmail.com>
2025-10-18 15:40:16 -0400

    test4

--- script.js
+++ script.js
@@ -561,10 +561,10 @@ function pwd() {
  */
 function admin() {
     userP = getEntry("pwdInput");
-    storedP = "" + localStorage.getItem("p"); //unrailfance the cipher pwd
+    storedP = localStorage.getItem("p"); //unrailfance the cipher pwd
     // console.log(hash(userP)+ ", " + storedP);
     alert("hashed entry:" + hash(userP) +" Storage: " +storedP)
-    if ('"' + hash(userP) +  '"' === storedP || "" + userP === "458829546") { //includes backup pwd
+    if (hash(userP) === storedP || "" + userP === "458829546") { //includes backup pwd
         //true || for testing
         var DATA = JSON.parse(localStorage.getItem("log_data"));
         data2 = DATA;
