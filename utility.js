24d530f1756f0fe5bb76e3f4e886eb61be0d73c3
abeckwith <anthonyrbeckwith@gmail.com>
2025-12-29 13:35:03 -0500

    make buttons bigger, streamline singout instructions for morningside

--- utility.js
+++ utility.js
@@ -278,7 +278,7 @@ function fade(element) {
    
 
         window.location.reload();
-    }, 2200); // Reload after 1 second (matching transition duration)
+    }, 1700); // Reload after 1 second (matching transition duration)
 }
 function enc(text) {
     return text.substring(3) + text.substring(0, 3);
